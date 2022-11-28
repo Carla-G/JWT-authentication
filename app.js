8b45077a114263cf47ec52e5e471c83d2895931f
Carla Gonçalves <goncalves.carla1998@gmail.com>
2022-11-28 21:22:04 +0000

    JWT Authentication is working

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-const { hashPassword } = require("./auth.js");
 
 const express = require("express");
 
@@ -16,20 +15,32 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
+const { hashPassword, verifyPassword, verifyToken, verifyId } = require("./auth.js");
 
+// the public routes
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-const userHandlers = require("./userHandlers");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", hashPassword, userHandlers.postUser);
-app.put("/api/users/:id", userHandlers.updateUser);
-app.delete("/api/users/:id", userHandlers.deleteUser);
+
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
+
+//the routes to protect
+app.use(verifyToken); // authentication wall : verifyToken is activated for each route after this line
+
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
+app.put("/api/users/:id", verifyId, userHandlers.updateUser);
+app.delete("/api/users/:id", verifyId, userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
